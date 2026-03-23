a7873a8992503f833874ba57c4838fa85abc5784
koushik <kmalagannavar@gmail.com>
2026-03-23 21:12:45 +0530

    base url changed to /

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
- import svgr from "vite-plugin-svgr"
+import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/movie_maniac/",
+  base: "/",
   plugins: [react(), svgr()],
 })
